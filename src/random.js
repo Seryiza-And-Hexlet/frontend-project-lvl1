4988bccc9ce6878feae4d48d76067942609ccc38
Sergey Zaborovsky <seryiza@yandex.ru>
2021-04-05 21:43:51 +1100

    Исправлено взятие случайного элемента в функции randomItem

--- src/random.js
+++ src/random.js
@@ -1,2 +1,2 @@
 export const rangeRandom = (min, max) => Math.floor(min + Math.random() * (max + 1 - min));
-export const randomItem = (array) => array[rangeRandom(0, array.length)];
+export const randomItem = (array) => array[rangeRandom(0, array.length - 1)];
